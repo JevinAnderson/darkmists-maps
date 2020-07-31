00095b73bbc9f89089eae5968d5cf10d30a7ea43
Jevin Anderson <jevanderson@paypal.com>
2020-07-30 18:18:54 -0700

    placeholder

--- src/components/map/map-room.js
+++ src/components/map/map-room.js
@@ -1,7 +1,34 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-import './map-room.scss'
+import "./map-room.scss";
+
+const titleStore = {};
+
+const Arrow = ({ direction, exits }) =>
+  exits[direction] || true ? (
+    <a
+      className={`map-room-arrow-container ${direction}`}
+      href={`/${exits[direction]}.html`}
+    >
+      <i className={`map-room-arrow ${direction}`} />
+    </a>
+  ) : null;
+
+function formatTitle(title = "") {
+  if (titleStore[title]) {
+    return titleStore[title];
+  }
+
+  const result = title
+    .toUpperCase()
+    .split(" ")
+    .map((word) => word.split("")[0])
+    .join("");
+  titleStore[title] = result;
+
+  return result;
+}
 class MapRoom extends Component {
   state = {};
 
@@ -19,25 +46,33 @@ class MapRoom extends Component {
 
   render() {
     if (this.props.placeholder) {
-      return <div className="map-room" />;
+      return <div className="map-room map-room-placeholder" />;
     }
     return (
-      <div className="map-room" id={this.props.vnum}>
-        <h4 className="map-room-title">{this.props.title}</h4>
-        <p className="map-room-vnum">VNUM: {this.props.vnum}</p>
-        <p className="map-room-exit-header">Exits: </p>
-        <p className="map-room-exits">
-          {Object.keys(this.props.exits).map((key) => (
-            <a
-              key={key}
-              href={`/${this.props.exits[key]}.html`}
-              className="map-room-exit"
-              style={{ display: "inline", paddingRight: "0.25rem" }}
-            >
-              {key}
-            </a>
-          ))}
-        </p>
+      <div className="map-room map-room-actual" id={this.props.vnum}>
+        <h4 className="map-room-title">{formatTitle(this.props.title)}</h4>
+        {/* <p className="map-room-vnum">VNUM: {this.props.vnum}</p> */}
+        {/* <p className="map-room-exit-header">Exits: </p> */}
+        <div className="map-room-exits">
+          <div className="map-room-cardinal-container">
+            {["east", "south", "west", "north"].map((direction) => (
+              <Arrow
+                key={direction}
+                direction={direction}
+                exits={this.props.exits}
+              />
+            ))}
+          </div>
+          <div className="map-room-vertical-container">
+            {["up", "down"].map((direction) => (
+              <Arrow
+                key={direction}
+                direction={direction}
+                exits={this.props.exits}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
